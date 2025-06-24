eee24d8cf80955c42cc941338232d953ec3e76dd
agilibert <armel.gilibert@gmail.com>
2025-06-24 10:28:12 +0200

    Debugging prisma new user

--- backend/src/signup.ts
+++ backend/src/signup.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify';
+import fastifyMultipart from '@fastify/multipart';
 import { PrismaClient } from '@prisma/client';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,24 +9,37 @@ const prisma = PrismaClient();
 
 //Add avatar file management with fastify-multipart
 
+function getFieldValue(field: any): string | undefined {
+	if (!field) return undefined;
+	if (Array.isArray(field)) field = field[0];
+}
+
 export async function registerNewUser(app: FastifyInstance) {
+	app.register(fastifyMultipart);
 	app.post('/api/signup', async (request, reply) => {
 		const data = await request.file();
-		const { username, password } = data.fields;
-		const avatarFile = data.file;
-		const hashedPassword = await bcrypt.hash(password.value, 10);
-
-		try {
-			await prisma.user.create({
-				data: {
-					username: username.value,
-					passwordHash: hashedPassword,
-					//avatarUrl: avatarFile,
-				}
-			});
-			reply.code(200).send({ sucess: true });
-		} catch {
-			reply.code(400).send({ error: "Failed to import User data to the DB" });
+		if (data){
+			const { username, password } = data.fields;
+			const usernameValue = Array.isArray(username) ? username[0].value : username.value;
+			const passwordValue = Array.isArray(password) ? password[0].value : password.value;
+			if (!usernameValue || !passwordValue){
+				reply.code(400).send({ error: "Invalid user info: missing username or password." });
+				return ;
+			}
+			const avatarFile = data.file;
+			const hashedPassword = await bcrypt.hash(passwordValue, 10);
+			try {
+				await prisma.user.create({
+					data: {
+						username: usernameValue,
+						passwordHash: hashedPassword,
+						//avatarUrl: avatarFile,
+					}
+				});
+				reply.code(200).send({ sucess: true });
+			} catch {
+				reply.code(400).send({ error: "Failed to import User data to the DB" });
+			}
 		}
 	});
 }
