9b4ae978d74cb940c27182a755262380abc633fa
agilibert <armel.gilibert@gmail.com>
2025-07-01 16:20:37 +0200

    Wroking version

--- backend/dist/login.js
+++ backend/dist/login.js
@@ -4,23 +4,21 @@ const secretKey = 'abcde12345';
 export async function handleLogIn(app, prisma) {
     console.log("DEBUG LOGIN MANAGEMENT");
     app.post("/api/login", async (request, reply) => {
-        try {
-            const { username, password } = request.body;
-            const user = await prisma.user.findUnique({ where: { username } });
-            if (!user)
-                return reply.status(401).send({ success: false, message: "User not found" });
-            const passwordCheck = await bcrypt.compare(password, user.passwordHash);
-            if (!passwordCheck)
-                return reply.status(401).send({ success: false, message: "Wrong password" });
-            reply.send({ success: true });
-        }
-        catch (err) {
-            console.error(err);
-            reply.status(500).send({ success: false, message: "Internal server error" });
-        }
+        const { username, password } = request.body;
+        console.log(username);
+        console.log(password);
+        const user = await prisma.user.findUnique({
+            where: { username }
+        });
+        if (!user)
+            return reply.status(401).send({ success: false, message: "User not found" });
+        const passwordCheck = await bcrypt.compare(password, user.passwordHash);
+        if (!passwordCheck)
+            return reply.status(401).send({ success: false, message: "Wrong password" });
+        //const token = generateJWT(username,prisma);
+        reply.send({ success: true });
     });
 }
-
 async function generateJWT(username, prisma) {
     const user = await prisma.user.findUnique({
         where: { username }
