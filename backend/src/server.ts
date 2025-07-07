e74a7380c790669dbd47568e3b41f448cbafcd33
Glasck <glasckint@gmail.com>
2025-07-07 11:49:12 +0200

    init

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -67,8 +67,8 @@ await chatWebSocketRoutes(app);
 
 const start = async () => {
 	try {
-		await app.listen({ port: 3001, host: "0.0.0.0" });
-		console.log(`App is listening on port: 3001`);
+		await app.listen({ port: 3000, host: "0.0.0.0" });
+		console.log(`App is listening on port: 3000`);
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
