7cee331dec19bbc83612e05738210e5f1813d34d
agiliber <armel.gilibert@gmail.com>
2025-07-15 13:58:57 +0000

    User game data fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,11 +22,11 @@ export default defineConfig({
 		},
 		proxy: {
 			"/api": {
-				target: "http://localhost:3000",
+				target: "http://localhost:3002",
 				changeOrigin: true,
 			},
 			"/ws": {
-				target: "ws://localhost:3000",
+				target: "ws://localhost:3002",
 				ws: true,
 				changeOrigin: true,
 			},
