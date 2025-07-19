e5f33823c796efeb7ca2331b37007b5e0c279806
ychun816 <ychun8lin@gmail.com>
2025-07-19 20:25:49 +0200

    setting up 2fa structure files, doing route/two-factor.ts, to test tomorrow

---
+++ backend/src/email.ts
@@ -0,0 +1,4 @@
+//backend/src/utils/email.ts
+//Helper for sending emails (e.g., using nodemailer).
+//Keeps email logic reusable and separate from 2FA logic.
+
---
+++ backend/src/services/twoFactorService.ts
@@ -0,0 +1,3 @@
+//All logic for generating codes, sending emails, and verifying codes.
+// Keeps your business logic separate from your route handlers.
+
