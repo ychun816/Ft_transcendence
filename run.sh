487e5063cf612009641b34e0b841eef160251ff3
Arthur <arthurbernard92000@gmail.com>
2025-08-03 12:46:59 +0200

    version casi-fini

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker compose -f .devcontainer/docker-compose.yml up --build
+
+export PUBLIC_IP=$(ip a | grep 'inet 10\.' | awk '{print $2}' | cut -d/ -f1 | head -n 1) && docker compose -f .devcontainer/docker-compose.yml up --build
