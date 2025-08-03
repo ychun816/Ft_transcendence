941a4e4237296c322b4eb56728471cfa99d6c07e
Arthur <arthurbernard92000@gmail.com>
2025-08-03 12:06:46 +0200

    ALERT + merge JOJO

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Single command Docker launch pour ft_transcendence - Conforme aux exigences du sujet
-docker compose -f .devcontainer/docker-compose.yml up prod --build
+docker compose -f .devcontainer/docker-compose.yml up --build
