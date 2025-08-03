86f11e8aef9c82bed27590a238e5e70fbff886bf
agiliber <armel.gilibert@gmail.com>
2025-08-03 19:08:07 +0200

    runsh correct

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export PUBLIC_IP=$(ip a | grep 'inet 10\.' | awk '{print $2}' | cut -d/ -f1 | head -n 1) && docker-compose --f .devcontainer/docker-compose.yml up --build
+export PUBLIC_IP=$(ip a | grep 'inet 10\.' | awk '{print $2}' | cut -d/ -f1 | head -n 1) && docker-compose -f .devcontainer/docker-compose.yml up --build
